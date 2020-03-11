0dcac9c9055375fb83aa981512ce1ed9b0cab8e2
ymsk.sky <ymsk.sky.95@gmail.com>
2020-03-11 22:35:21 +0900

    write example script on main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import speedtest
+
+servers = []
+threads = None
+
+s = speedtest.Speedtest()
+s.get_servers(servers)
+s.get_best_server()
+
+s.download(threads=threads)
+s.upload(threads=threads)
+
+s.results.share()
+
+results_dict = s.results.dict()
