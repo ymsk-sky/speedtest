53f4702e1523cd874729f32154f376d0d2a860dc
ymsk.sky <ymsk.sky.95@gmail.com>
2020-03-12 21:14:53 +0900

    show result simple

--- main.py
+++ main.py
@@ -16,4 +16,7 @@ s.results.share()
 
 results_dict = s.results.dict()
 
-print(results_dict)
+# print(results_dict)
+print('Download: {}Mbps'.format(results_dict['download']/1024/1024))
+print('Upload: {}Mbps'.format(results_dict['upload']/1024/1024))
+print('ping: {}ms'.format(results_dict['ping']))
